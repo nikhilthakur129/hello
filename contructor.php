11cbe903088a5bc11c4576e982088378798b62b5
nikhilthakur129 <62504874+nikhilthakur129@users.noreply.github.com>
2020-10-11 19:46:52 +0530

    Update contructor.php

--- contructor.php
+++ contructor.php
@@ -35,4 +35,6 @@ echo"<br>**********THANK YOU***********";
 $obj=new employee("Sam","Assistant professor",20000);
 $obj->netsalcalculate();
 $obj->show();
+echo"<br>**********THANK YOU***********";
 ?>
+
